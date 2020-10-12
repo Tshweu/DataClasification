c23e13a02846af5a6465431559f5d82f0ec90f63
Tshweu <t.t.sephiri@gmail.com>
2020-10-12 20:09:43 +0200

    added login and signup routes and link active

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,8 +3,10 @@ import { Routes, RouterModule } from '@angular/router';
 import { LoginComponent } from './login/login.component';
 import { RegisterComponent } from './register/register.component';
 
-const routes: Routes = [{path:"",component:LoginComponent},
-{path:"register",component:RegisterComponent}
+const routes: Routes = [
+  {path:"",component:LoginComponent},
+  {path:"login",component:LoginComponent},
+  {path:"register",component:RegisterComponent}
 ];
 
 @NgModule({
