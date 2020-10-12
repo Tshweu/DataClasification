22d2d825099998a39d6d16d1d5224970dec0ff98
Tshweu <t.t.sephiri@gmail.com>
2020-10-12 23:50:04 +0200

    added routing for home and landing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,12 +1,15 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { LandingComponent } from './landing/landing.component';
 import { LoginComponent } from './login/login.component';
 import { RegisterComponent } from './register/register.component';
 
 const routes: Routes = [
-  {path:"",component:LoginComponent},
+  {path:"",component:LandingComponent},
   {path:"login",component:LoginComponent},
-  {path:"register",component:RegisterComponent}
+  {path:"register",component:RegisterComponent},
+  {path:"home",component:HomeComponent}
 ];
 
 @NgModule({
