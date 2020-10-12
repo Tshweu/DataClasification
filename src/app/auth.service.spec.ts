0f2e1bbf171a338a63d1bc6c6045b2ab73292801
Tshweu <t.t.sephiri@gmail.com>
2020-10-12 20:57:13 +0200

    updated service tests

--- src/app/auth.service.spec.ts
+++ src/app/auth.service.spec.ts
@@ -1,9 +1,15 @@
 import { TestBed } from '@angular/core/testing';
-
+import { HttpClientTestingModule, HttpTestingController } from '@angular/common/http/testing';
+import {HttpClientModule} from '@angular/common/http';
 import { AuthService } from './auth.service';
 
 describe('AuthService', () => {
   let service: AuthService;
+
+  beforeEach(() => TestBed.configureTestingModule({
+    imports: [HttpClientTestingModule], 
+    providers: [AuthService]
+  }));
 
   beforeEach(() => {
     TestBed.configureTestingModule({});
