0e936d3a5b5b2a1c7c373150d5f99dff510f568f
Tshweu <t.t.sephiri@gmail.com>
2020-10-13 01:35:15 +0200

    added intercept to add token to headers

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -11,10 +11,11 @@ import { RegisterComponent } from './register/register.component';
 import { NavbarComponent } from './navbar/navbar.component';
 import { FormsModule } from '@angular/forms';
 import { AuthService } from './auth.service';
-import { HttpClientModule } from '@angular/common/http';
+import { HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http';
 import { HomeComponent } from './home/home.component';
 import { LandingComponent } from './landing/landing.component';
 import { AuthGuard } from './auth.guard';
+import { TokenInterceptorService } from './token-interceptor.service';
 
 
 @NgModule({
@@ -35,7 +36,13 @@ import { AuthGuard } from './auth.guard';
     HttpClientModule
   ],
   schemas: [NO_ERRORS_SCHEMA],
-  providers: [AuthService, AuthGuard],
+  providers: [AuthService, AuthGuard, 
+  {
+    provide: HTTP_INTERCEPTORS,
+    useClass: TokenInterceptorService,
+    //allows use of multiple interceptors
+    multi: true
+  }],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
