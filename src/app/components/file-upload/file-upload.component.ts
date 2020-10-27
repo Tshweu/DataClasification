8b743f4f922c313fb3d38a4d0ec72803a48eb2a6
Tshweu <t.t.sephiri@gmail.com>
2020-10-27 05:38:48 +0200

    file upload component injects service and uploads

--- src/app/components/file-upload/file-upload.component.ts
+++ src/app/components/file-upload/file-upload.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FileUploadService } from '../../services/file-upload.service';
 
 @Component({
   selector: 'app-file-upload',
@@ -6,14 +7,28 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./file-upload.component.scss']
 })
 export class FileUploadComponent implements OnInit {
+  selectedFile: File = null;
 
-  constructor() { }
+  constructor(private _fileUploadService: FileUploadService) { }
 
   ngOnInit(): void {
   }
 
+  //event is passed to func
+  //selected file is set to value of the selected file
   onFileSelected(event){
-    console.log(event);
+    this.selectedFile = <File>event.target.files[0];
+    console.log(this.selectedFile);
+  }
+  //On upload button click
+  //file service file upload function is passed file
+  //then subscribed to, to get the return value 
+  uploadFile(){
+    this._fileUploadService.fileUpload(this.selectedFile)
+      .subscribe(
+        res => console.log(res),
+        err => console.log(err),
+      )
   }
 
 }
