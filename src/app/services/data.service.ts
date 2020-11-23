f3d8fb782d64e11596399212d2c7f6904f2cb7e6
Tshweu <t.t.sephiri@gmail.com>
2020-11-23 16:04:09 +0200

    service to pass meta data between components

---
+++ src/app/services/data.service.ts
@@ -0,0 +1,17 @@
+import { Injectable } from '@angular/core';
+import { BehaviorSubject } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class DataService {
+  private data = new BehaviorSubject<any>({});
+  currentData = this.data.asObservable();
+  constructor() { }
+
+  updateData(data){
+    this.data.next(data);
+  }
+
+  //missionAnnounced$ = this.announced.asObservable();
+}
